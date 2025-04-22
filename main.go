5d2f9813aaf870cb4cb589e3c9eddf77494e8c7d
Karthik Gowda L <106536496+Kgl-01@users.noreply.github.com>
2025-04-22 06:16:12 +0000

    using type as an alias for exiting dataType

--- main.go
+++ main.go
@@ -2,6 +2,14 @@ package main
 
 import "fmt"
 
+type customString string
+
+func (c *customString) printString() {
+	deReferencedString := *c
+	fmt.Println(deReferencedString)
+}
+
 func main() {
-	fmt.Println("Hello World")
+	var name customString = "Karthik Gowda"
+	name.printString()
 }
